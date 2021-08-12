2e9a0cccae877634204ed80ca655caa248618c2a
devisap <ilham.sagitaputra@gmail.com>
2021-08-12 15:03:27 +0700

    get list lahan terdekat

--- application/models/LahanModel.php
+++ application/models/LahanModel.php
@@ -23,6 +23,32 @@ class LahanModel extends CI_Model{
         $res    = $this->db->order_by('TGLULASAN_URBAN', 'desc')->get_where('v_lahan_ulasan', $filter)->result();
         return $res;
     }
+    public function getTerdekat($param){
+        $limit = $param['limit'] != '-1' ? 'LIMIT '.$param['limit'] : '';
+        $res = $this->db->query('
+            SELECT 
+            l.ID_LAHAN ,
+            l.NAMA_LAHAN ,
+            ST_DISTANCE_SPHERE(
+                point('.$param['long'].', '.$param['lat'].'),
+                point(l.LONGITUDE_LAHAN , l.LATITUDE_LAHAN)
+            )/1000 as DISTANCE,
+            mp.NAMA_PROVINSI ,
+            mk.NAMA_KOTA ,
+            mk2.NAMA_KECAMATAN ,
+            mk3.NAMA_KELURAHAN ,
+            l.ALAMAT_LAHAN ,
+            l.LATITUDE_LAHAN ,
+            l.LONGITUDE_LAHAN 
+            FROM lahan l, md_provinsi mp , md_kota mk , md_kecamatan mk2 , md_kelurahan mk3 
+            WHERE 
+            l.ID_PROVINSI = mp.ID_PROVINSI AND l.ID_KOTA = mk.ID_KOTA AND l.ID_KECAMATAN = mk2.ID_KECAMATAN AND l.ID_KELURAHAN = mk3.ID_KELURAHAN 
+            ORDER BY DISTANCE ASC
+            '.$limit.'
+        ')->result();
+
+        return $res;
+    }
     public function insert($param){
         $this->db->insert('user', $param);
         return $this->db->insert_id();
